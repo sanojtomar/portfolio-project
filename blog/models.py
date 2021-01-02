2c6c25209c3890728fbeba17479583331be5f09b
Sanoj Kumar <sanojtomar@gmail.com>
2021-01-02 12:36:22 +0530

    Added bootstrap files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Blog(models.Model):
+	title = models.CharField(default='',max_length=200)
+	body = models.TextField(default='')
+	pub_date = models.DateTimeField(default=datetime.now(), blank=True)
+	image = models.ImageField(default='',upload_to='images/')
+
+
