fdec6782bece31d72e174e3d9e79c2a371033ebd
Sanoj Kumar <sanojtomar@gmail.com>
2021-01-04 15:40:18 +0530

    Final code for portfolio

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,12 @@ class Blog(models.Model):
 	body = models.TextField(default='')
 	pub_date = models.DateTimeField(default=datetime.now(), blank=True)
 	image = models.ImageField(default='',upload_to='images/')
+	
+	def summary(self):
+		return self.body[:100]
+	
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
 
-
+	def __str__(self):
+		return self.title 
